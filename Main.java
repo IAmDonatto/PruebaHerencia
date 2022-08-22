fc7144d0318cb35f0ac4062045a513ae9b8cc8f1
IAmDonatto <110427934+IAmDonatto@users.noreply.github.com>
2022-08-22 13:41:00 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebaherencia;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author PC
+ */
+public class Main {
+    public class main{
+        public static void main (String [] args){
+            String res="";
+            int resN= 0;
+            vehiculo vehiculo= new vehiculo();
+            JOptionPane.showMessageDialog(null,"");
+        }
+    }
+    
+}
